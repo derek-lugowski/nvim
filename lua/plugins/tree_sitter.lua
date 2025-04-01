0948020f9950bf29d3dad4f4433b4ea9897b1a0f
Derek Lugowski <derek.lugowski@gmail.com>
2025-04-01 09:47:28 -0600

    Add treesitter

---
+++ lua/plugins/tree_sitter.lua
@@ -0,0 +1,8 @@
+return {
+     "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    dependencies = {
+        "windwp/nvim-ts-autotag",
+    },
+}
