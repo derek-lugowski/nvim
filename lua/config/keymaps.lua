814116c24c3ea55a7455e716e62c92a2e5d1bfac
Derek Lugowski <derek.lugowski@gmail.com>
2025-04-02 14:08:20 -0600

    Add keymaps for moving between vertical splits

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -18,5 +18,7 @@ map("n", "gd", lsp.definition, {desc = "Go to definition"})
 -- Moving between window splits
 map("n", "<C-k>", "<cmd>wincmd k<cr>", {desc = "move to top split"})
 map("n", "<C-j>", "<cmd>wincmd j<cr>", {desc = "move to bottom split"})
+map("n", "<C-h>", "<cmd>wincmd h<cr>", {desc = "move to left split"})
+map("n", "<C-l>", "<cmd>wincmd l<cr>", {desc = "move to right split"})
 map("n", "<C-s>", "<cmd>wincmd v<cr>", {desc = "Split file vertically"})
 
