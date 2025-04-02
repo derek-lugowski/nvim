f65c8238f1ae5fde7c1cf3450590de54dd2ce60f
Derek Lugowski <derek.lugowski@gmail.com>
2025-04-02 12:06:18 -0600

    Add remaps for window splits

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,9 +3,15 @@ local map = vim.keymap.set
 map("n", "<Leader>..", "<cmd>Ex<cr>")
 
 -- Telescope
-map("n", "<Leader>ff", "<cmd>Telescope find_files<cr>")
-map("n", "<Leader>fg", "<cmd>Telescope live_grep<cr>")
+local telescope = require("telescope.builtin")
+map("n", "<Leader>ff", telescope.find_files, {desc = "Telescope find files"})
+map("n", "<Leader>fg", telescope.live_grep, {desc = "Telescope live grep"})
 
 -- LSP
-map("n", "<Leader>k", vim.lsp.buf.hover, {})
-map("n", "gd", vim.lsp.buf.definition, {})
+local lsp = vim.lsp.buf
+map("n", "<Leader>k", lsp.hover, {desc = "LSP Hover"})
+map("n", "gd", lsp.definition, {desc = "Go to definition"})
+
+-- Moving between window splits
+map("n", "<C-k>", "<cmd>wincmd k<cr>", {desc = "move to top split"})
+map("n", "<C-j>", "<cmd>wincmd j<cr>", {desc = "move to bottom split"})
