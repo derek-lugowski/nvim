9e54def2ad395774158759b412ee34161d998d5c
Derek Lugowski <derek.lugowski@gmail.com>
2025-04-01 22:47:18 -0600

    Setup lsp

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,7 @@ vim.keymap.set("n", "<Leader>..", "<cmd>Ex<cr>")
 -- Telescope
 vim.keymap.set("n", "<Leader>ff", "<cmd>Telescope find_files<cr>")
 vim.keymap.set("n", "<Leader>fg", "<cmd>Telescope live_grep<cr>")
+
+-- LSP
+vim.keymap.set("n", "<Leader>k", vim.lsp.buf.hover, {})
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
