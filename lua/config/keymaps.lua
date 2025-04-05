1d952b20f76177402326cd5e777f323ab049c49f
Derek Lugowski <derek.lugowski@gmail.com>
2025-04-05 11:56:56 -0600

    Add go to references keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,7 +8,7 @@ map("n", "<Leader>ff", telescope.find_files, { desc = "Telescope find files" })
 map("n", "<Leader>fg", telescope.live_grep, { desc = "Telescope live grep" })
 map("n", "<Leader>fs", telescope.grep_string, { desc = "Telescope grep string" })
 map("n", "<Leader>fn", function()
-	telescope.find_files({ cwd = "~/.config/nvim" })
+    telescope.find_files({ cwd = "~/.config/nvim" })
 end, { desc = "Telescope find nvim config files" })
 map("n", "<Leader>fi", telescope.git_files, { desc = "Telescope find files tracked by git" })
 
@@ -16,6 +16,7 @@ map("n", "<Leader>fi", telescope.git_files, { desc = "Telescope find files track
 local lsp = vim.lsp.buf
 map("n", "<Leader>k", lsp.hover, { desc = "LSP Hover" })
 map("n", "gd", lsp.definition, { desc = "Go to definition" })
+map('n', 'gr', telescope.lsp_references, { desc = "List references to the symbol under the cursor" })
 
 -- Moving between window splits
 map("n", "<C-k>", "<cmd>wincmd k<cr>", { desc = "move to top split" })
